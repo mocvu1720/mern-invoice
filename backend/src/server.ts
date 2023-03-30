4a298dea6ecbe86db4c418edf16af29cb65eeb08
mocvu1720 <nguyenhuuhuy170820@gmail.com>
2023-03-30 23:14:25 +0700

    setup nginx reverse proxy with docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
+app.use(morganMiddleware);
+
 app.get("/app/v1/test", (req, res) => {
   res.json({ message: "Hello World" });
 });
@@ -24,4 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port ${chalk.yellow.bold(PORT)}`);
+  systemLogs.info(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
